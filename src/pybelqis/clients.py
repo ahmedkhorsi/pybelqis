1ab2451f03fa732690fe56c518230e758afabfd9
Ahmed Khorsi <ahmedkhorsi18@gmail.com>
2023-02-27 20:36:19 +0300

    Cleanup

--- src/pybelqis/clients.py
+++ src/pybelqis/clients.py
@@ -1,4 +1,3 @@
-import requests
 from gql.transport.aiohttp import AIOHTTPTransport
 from gql import Client, gql
 
@@ -17,51 +16,3 @@ class Belqis:
         ''')
         return self.client.execute(query)['tashkeel']
 
-    # These are temporarily suspended services. Maybe soon made available IN SHAA ALLAH 
-    # def ner(self, text):
-    #     query = gql(f'''
-    #         query {{
-    #             ner(text: "{text}"){{
-    #                 token
-    #                 tag
-    #             }}
-    #         }}
-    #     ''')
-    #     return self.client.execute(query)['ner']
-
-    # def dictate(self, audio_url):
-    #     query = gql(f'''
-    #         mutation Dictate($audio: Upload!){{
-    #             dictate(audio: $audio){{
-    #                 text
-    #             }}
-    #         }}
-    #     ''')
-    #     with open(audio_url, 'rb') as f:
-    #         result = self.client.execute(query, variable_values={'audio': f}, upload_files=True)
-    #     return result['dictate']['text']
-
-    # def pronounce(self, audio_url, ref_text):
-    #     query = gql(f'''
-    #         mutation Pronounce($audio: Upload!,$refText: String!){{
-    #             pronounce(audio: $audio, refText: $refText){{
-    #                 mistakes
-    #                 predicted
-    #             }}
-    #         }}
-    #     ''')
-    #     with open(audio_url, 'rb') as f:
-    #         result = self.client.execute(query, variable_values={'audio': f, 'refText': ref_text}, upload_files=True)
-    #     return result['pronounce']
-
-
-    def send(self):
-        try:
-            res = requests.post(
-                URL, json={'query': self.query}, headers=self.headers, verify=True)
-
-            if res.ok:
-                return res.json()
-            return {'errors': [{'message': res.reason}]}
-        except Exception as e:
-            return {'errors': [{'message': str(e)}]}
